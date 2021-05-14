b80b640795842aec31ed9e4a2404948daa2b3941
lauraescobarc <66136705+lescobarc@users.noreply.github.com>
2021-05-14 11:50:18 -0500

    S12-175 Mostrar Rutas Hijas - Segundo RouterOutlet

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -10,7 +10,7 @@ import { HomeComponent } from './pages/home/home.component';
 const routes: Routes =[
   {
     path: '',
-    
+    component: HomeComponent,
     children: [
       {
         path: 'listado',
