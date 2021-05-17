fcaaa732f9d64b59270a6d75467fc3e4d63941d1
lauraescobarc <66136705+lescobarc@users.noreply.github.com>
2021-05-17 17:59:32 -0500

    S13-189 TAREA - PipeImagen

--- src/app/heroes/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/heroes/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -14,5 +14,6 @@ export class HeroeTarjetaComponent implements OnInit {
   }
 
   @Input() heroe!: Heroe
+ 
 
 }
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { ImagenPipe } from './pipes/imagen.pipe';
 
 
 
@@ -23,6 +24,7 @@ import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.
     HomeComponent,
     ListadoComponent,
     HeroeTarjetaComponent,
+    ImagenPipe,
     
   ],
   imports: [
