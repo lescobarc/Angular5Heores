eaa41dcd176b697f465dbd291edbfa8deddab5fa
lauraescobarc <66136705+lescobarc@users.noreply.github.com>
2021-05-17 19:22:19 -0500

    S13-192 Diseño de la pantalla de Héroe - función Botón regresar

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,13 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Heroe } from '../../interfaces/heroes.interface';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HeroesService } from '../../services/heroes.service';
 import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styleUrls: ['./heroe.component.css']
+  styleUrls: ['./heroe.component.css'],
+  styles: [`
+    img {
+      width: 100%;
+      border-radius: 5px;
+    }
+  `]
 })
 export class HeroeComponent implements OnInit {
 
@@ -15,7 +21,8 @@ heroe!: Heroe
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private heroesService: HeroesService
+    private heroesService: HeroesService,
+    private router: Router
     ) { }
 
   ngOnInit(): void {
@@ -25,6 +32,10 @@ heroe!: Heroe
       switchMap(({id})=> this.heroesService.getHeroePorId(id))
     )
    .subscribe (heroe =>this.heroe = heroe )
+  }
+
+  regresar() {
+    this.router.navigate(['/heroes/listado'])
   }
 
 }
